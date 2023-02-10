abccc2b3077f772e075ea1893c22e5200d1c62ae
เศรษฐโชค ตั้งศิริวัฒนวงศ์ <94209913+Kohcattes@users.noreply.github.com>
2023-02-11 00:44:01 +0700

    Update Style And Lower Line
    More comment and Edit Scope Method in class Stack

--- Stack_Lab.py
+++ Stack_Lab.py
@@ -1,62 +1,57 @@
 '''Stack Code Lab'''
 class Stack:
     '''Create a new stack Lab3'''
-    def __init__(self):
+    def __init__(self):#Use Auto to Creat Class
         self.data = []
     
     def size(self):
         '''size of stack'''
-        return len(self.data)
+        return len(self.data)#return
 
     def is_empty(self):
         '''Empty True or Not'''
-        if len(self.data) == 0:
-            return True
-        else:
-            return False
+        return (len(self.data) == 0)#Check Empty Stack
         
     def push(self, pNew):
         '''Push Data on top Stack'''
-        self.data.append(pNew)
+        self.data.append(pNew)#Input on Top Stack
         
     def pop(self):
         '''delete data from top Stack'''
         if len(self.data) == 0:
             return None
         else:
-            return self.data.pop()
+            return self.data.pop()#Delete Top Stack
             
     def stackTop(self):
         '''Top Stack'''
         if len(self.data) == 0:
             return None
         else:
-            return self.data[-1]
+            return self.data[-1]#Check Latest Input
     
     def printStack(self):
         '''print member'''
-        print(self.data)
+        print(self.data)#Output Stack On Screen
 
+dicOper = {'*':2, '/': 2, '+':1, '-' :1} #Use in 4.3 postfix
+        
 def is_parentheses_matching(exp):
     '''Check "(", ")" matches Lab4.1'''
-    first = exp.count('(')
-    end = exp.count(')')
-    if first == end:
+    condi = (exp.count(')') == exp.count('('))
+    if condi:
         print('Parentheses in '+exp+' are matched')
-        return True
     else:
         print('Parentheses in '+exp+' are unmatched')
-        return False
+    return condi
 
 def copyStack(stack1, stack2):
     '''Copy Stack 4.2'''
     for i in stack1.data:
         stack2.push(i)
 
-dicOper = {'*':2, '/': 2, '+':1, '-' :1} #use in 4.3 postfix
-
 def infixToPostfix(exp):
-    '''infixToPostfix Lab4.3'''
+    '''infix To Postfix Lab4.3'''
     stack1 = Stack()
     pnew = []
     for i in exp:
